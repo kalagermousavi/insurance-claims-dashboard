8dc34ac44462565acb9c02c65df293d90f104d29
اseyede kimia mousavi kalager <seyedekimiamousavikalager@gmail.com>
2026-07-23 23:24:19 +0330

    Add notification button interaction

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+console.log("ClaimFlow JavaScript is connected!");
+const notificationBtn = document.querySelector("#notification-btn");
+notificationBtn.addEventListener("click", function () {
+  console.log("Notification button clicked!");
+  alert("You have new notifications!");
+});
