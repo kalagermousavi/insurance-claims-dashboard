c20a581dc278f77fad21b5b2e1c90840c9aad655
اseyede kimia mousavi kalager <seyedekimiamousavikalager@gmail.com>
2026-07-25 22:16:22 +0330

    Implement search input value tracking

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,10 @@ console.log("ClaimFlow JavaScript is connected!");
 
 const notificationBtn = document.querySelector("#notification-btn");
 const notificationPanel = document.querySelector("#notification-panel");
+const searchInput = document.querySelector("#search-input");
+searchInput.addEventListener("input", function () {
+  console.log(searchInput.value);
+});
 
 notificationBtn.addEventListener("click", function () {
   console.log("Notification button clicked!");
