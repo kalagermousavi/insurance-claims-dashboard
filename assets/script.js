d06669d59723e1df7ae19120136bdaac2cb26ae2
اseyede kimia mousavi kalager <seyedekimiamousavikalager@gmail.com>
2026-08-05 22:34:07 +0330

    feat: complete date filter functionality

--- assets/script.js
+++ assets/script.js
@@ -127,6 +127,19 @@ dateFilter.addEventListener("change", function () {
   });
 
   console.log("Filtered Claims:", filteredClaims);
+  claimsTableBody.innerHTML = "";
+  filteredClaims.forEach(function (claim) {
+    claimsTableBody.innerHTML += `
+    <tr>
+      <td>${claim.id}</td>
+      <td>${claim.customer}</td>
+      <td>${claim.insuranceNo}</td>
+      <td>${claim.vehicle}</td>
+      <td>${claim.status}</td>
+      <td>${claim.date}</td>
+    </tr>
+  `;
+  });
 });
 
 // Notification button
